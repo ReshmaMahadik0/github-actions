9592c399af580d6475cf4ef9b026f6ed09551be0
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-04 12:44:21 +0530

    code

---
+++ github-actions-example/src/main/java/com/example/github_actions_example/controller/Controller.java
@@ -0,0 +1,13 @@
+package com.example.github_actions_example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome to java";
+    }
+}
