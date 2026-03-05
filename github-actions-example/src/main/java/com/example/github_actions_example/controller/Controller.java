e38d2c0a802941d735561e76514a4aa9664d083f
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-05 22:23:29 +0530

    build

--- github-actions-example/src/main/java/com/example/github_actions_example/controller/Controller.java
+++ github-actions-example/src/main/java/com/example/github_actions_example/controller/Controller.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
+
     @GetMapping("/welcome")
     public String welcome(){
         return "welcome to java";
