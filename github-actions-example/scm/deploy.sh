d1cdff798b9096c4accd8e57d876f5ef046640e0
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-05 22:16:33 +0530

    build

--- github-actions-example/scm/deploy.sh
+++ github-actions-example/scm/deploy.sh
@@ -1,5 +1,11 @@
 echo "Step 4: Building Docker image"
 docker build -t github-actions-example .
 
-echo "Step 5: Running Docker container"
-docker run -d -p 8080:8080 github-actions-example
+echo "Step 5: Stop old container if running"
+docker stop github-actions-container || true
+docker rm github-actions-container || true
+
+echo "Step 6: Running Docker container"
+docker run -d -p 8080:8080 --name github-actions-container github-actions-example
+
+echo "Deployment completed"
