95eb4bc47bf9dc5a3f04a43ae4b5476dbb39d817
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-07 23:33:07 +0530

    build

--- github-actions-example/scm/test.sh
+++ github-actions-example/scm/test.sh
@@ -1,7 +1,15 @@
-cd "$(dirname "$0")/.."
-
-echo "Step 2: Run Tests"
-./mvn test
-used maven wrapper instead of system wrapper
+#cd "$(dirname "$0")/.."
+#
+#echo "Step 2: Run Tests"
+#./mvn test
+#used maven wrapper instead of system wrapper
 
 #mvn test
+
+cd "$(dirname "$0")/.."
+
+chmod +x mvnw
+
+echo "Step 2: Run Tests"
+./mvnw test
+#This moves execution to the project root where mvnw exists.
