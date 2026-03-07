d1ef63c54df12ad46a9570496e44927efcff093f
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-07 19:13:19 +0530

    build

--- github-actions-example/scm/deploy.sh
+++ github-actions-example/scm/deploy.sh
@@ -1,15 +1,16 @@
 
-#docker build -t github-actions-example .
+
 
 #cd "$(dirname "$0")/.."
 #
 echo "Docker Username: $DOCKER_USERNAME"
 
 echo "Build Docker Image"
-docker build -t $DOCKER_USERNAME/github-actions-example .
+docker build -t $DOCKER_USERNAME/spring-boot-demo:latest .
 echo "Push Docker Image"
-docker push $DOCKER_USERNAME/github-actions-example
+docker push $DOCKER_USERNAME/spring-boot-demo:latest
 echo "Run Docker Container"
-docker run -d -p 8080:8080 $DOCKER_USERNAME/github-actions-example
+docker run -d -p 8080:8080 $DOCKER_USERNAME/spring-boot-demo:latest
 
+#docker build -t github-actions-example .
 #docker run -d -p 8080:8080 github-actions-example
