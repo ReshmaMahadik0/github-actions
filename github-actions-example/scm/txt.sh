6d6401132aa94dafc845d86a09e0c034d42b1796
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-10 17:21:21 +0530

    sonarqube analysis

---
+++ github-actions-example/scm/txt.sh
@@ -0,0 +1,29 @@
+cd "$(dirname "$0")/.."
+
+
+echo "STEP 1 : Build Spring Boot Project"
+chmod +x mvnw
+./mvnw clean install -DskipTests
+
+
+echo "STEP 2 : Run Tests"
+./mvnw test
+
+
+echo "STEP 3 : SonarQube Analysis"
+./mvnw clean verify sonar:sonar \
+-Dsonar.token=$SONAR_TOKEN
+
+
+echo "STEP 4 : Docker Build"
+echo "Docker Username: $DOCKER_USERNAME"
+docker build -t "${DOCKER_USERNAME}/spring-boot-demo:latest" .
+
+
+echo "STEP 5 : Push Docker Image"
+docker push "${DOCKER_USERNAME}/spring-boot-demo:latest"
+
+echo "Run Docker Container"
+docker run -d -p 8080:8080 "${DOCKER_USERNAME}/spring-boot-demo:latest"
+
+
