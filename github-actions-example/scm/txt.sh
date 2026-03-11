1aa45cbe5a5ce97752abd2973d76b4c5adfb51d5
ReshmaMahadik0 <mahadikreshma408@gmail.com>
2026-03-11 22:24:27 +0530

    added configuration of ec2

--- github-actions-example/scm/txt.sh
+++ github-actions-example/scm/txt.sh
@@ -34,30 +34,33 @@ docker push $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
 #echo "Run Docker Container"
 #docker run -d -p 8080:8080 "${DOCKER_USERNAME}/spring-boot-demo:latest"
 
-#echo "================================="
-#echo "STEP 6 : Deploy to EC2"
-#echo "================================="
-#
-#echo "$EC2_SSH_KEY" > key.pem
-#chmod 400 key.pem
-#
+echo "================================="
+echo "STEP 6 : Deploy to EC2"
+echo "================================="
+
+echo "$EC2_SSH_KEY" > key.pem
+chmod 400 key.pem
+
 #ssh -o StrictHostKeyChecking=no -i key.pem ec2-user@$EC2_HOST << EOF
-#
-#aws ecr get-login-password --region $AWS_REGION \
-#| docker login --username AWS --password-stdin $ECR_REGISTRY
-#
-#docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest
-#
-#docker stop $CONTAINER_NAME || true
-#docker rm $CONTAINER_NAME || true
-#
-#docker run -d \
-#-p 80:8080 \
-#--name $CONTAINER_NAME \
-#$ECR_REGISTRY/$ECR_REPOSITORY:latest
-#
-#EOF
-#
-#echo "Deployment Completed"
-#
+
+aws ecr get-login-password --region $AWS_REGION \
+| docker login --username AWS --password-stdin $ECR_REGISTRY
+
+#pull the latest image
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:latest
+
+#Stop old container(if running)
+docker stop $CONTAINER_NAME || true
+docker rm $CONTAINER_NAME || true
+
+#Run new Container
+docker run -d \
+-p 80:8080 \
+--name $CONTAINER_NAME \
+$ECR_REGISTRY/$ECR_REPOSITORY:latest
+
+EOF
+
+echo "Deployment Completed"
+
 
